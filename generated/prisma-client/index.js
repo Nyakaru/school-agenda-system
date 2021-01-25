a0f285b5cd1a8e55336110eb6f27e79a1f2a327f
nyakaru <nyakaru477@gmail.com>
2021-01-26 00:43:33 +0300

    - setup develop branch

---
+++ generated/prisma-client/index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "Role",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://eu1.prisma.sh/kinara-moses-f425e2/hacker/dev`
+});
+exports.prisma = new exports.Prisma();
