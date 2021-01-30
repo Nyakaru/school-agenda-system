2c3b1424ec00836402b6a09e9a745abfeca93f22
Kinara Moses <Nyakaru477@gmail.com>
2021-01-30 14:40:02 +0300

    chore(transpoter): setup transporter (#6)
    - setup reusable nodemailer transporter
    - add sample templates
    - send a mail after signup

--- src/configs/index.js
+++ src/configs/index.js
@@ -2,4 +2,8 @@ require('dotenv').config();
 
 export const configs = {
     appSecret: process.env.APP_SECRET,
+    smtpHost: process.env.SMTP_HOST,
+    smtpPort: process.env.SMTP_PORT,
+    smtpEmail: process.env.SMTP_EMAIL,
+    smtpPass: process.env.SMTP_PASS,
 };
