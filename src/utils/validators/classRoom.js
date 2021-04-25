68a98af3d9f185242ae4db59990606ef1a8b8066
Kinara Moses <Nyakaru477@gmail.com>
2021-04-25 12:53:47 +0300

    chore - add classteacher (#22)

--- src/utils/validators/classRoom.js
+++ src/utils/validators/classRoom.js
@@ -4,4 +4,5 @@ import * as yup from 'yup';
 export const acceptableClassRoomPayload = yup.object().shape({
     name: yup.string().trim().required('Class name is required'),
     level: yup.string().trim().required('Class level is required'),
+    classTeacher: yup.object(),
 });
